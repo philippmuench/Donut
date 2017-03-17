69671d2dd67a7c56d59b17bdedda28a31e93f428
anon <philipp.muench@helmholtz-hzi.de>
2017-03-17 12:01:03 +0100

    extract band informations

--- fasta2karyo.sh
+++ fasta2karyo.sh
@@ -4,10 +4,13 @@
 # $2: ORF fasta file (prokka output)
 
 # process chr
-cat $1 | awk '$0 ~ ">" {print c; c=0;printf "chr - " substr($0,2,100) " " substr($0,2,100) " " "0" " "; } $0 !~ ">" {c+=length($0);} END { print c; }' > chr.txt
+echo "extract chr informations"
+cat $1 | awk '$0 ~ ">" {print c; c=0;printf "chr - " substr($0,2,100) " " substr($0,2,100) " " "0" " "; } $0 !~ ">" {c+=length($0);} END { print c; }' > ${1##*/}_chr.txt
 
 # process band
-# parse fasta header and extract contig information
-grep ">" $2 | awk '{print $1}' | sed 's/^.//' | awk -F'_' '{print $1}' > contig.txt
-grep ">" $2 | awk -F'#' '{print $2}' > start.txt 
-grep ">" $2 | awk -F'#' '{print $3}' > end.txt
+echo "extract band informations"
+grep ">" $2 | awk '{print $1}' | sed 's/^.//' | awk -F'_' '{print $1}' > ${2##*/}_contig.txt
+grep ">" $2 | awk -F'#' '{print $2}' > ${2##*/}_start.txt 
+grep ">" $2 | awk -F'#' '{print $3}' > ${2##*/}_end.txt
+
+# merge files to one karyofile
