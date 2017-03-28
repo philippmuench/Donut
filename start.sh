cc88d209f2083f202d5813dec40250a80fe630a5
anon <philipp.muench@helmholtz-hzi.de>
2017-03-28 15:27:08 +0200

    first entry vs all

--- start.sh
+++ start.sh
@@ -12,6 +12,7 @@ mkdir -p data/circos/hmmvis
 mkdir -p data/circos/fasta/
 mkdir -p data/circos/hmm
 mkdir -p data/circos/coverage
+mkdir -p data/circos/blast
 
 rm -rf data/circos/karyotype/
 mkdir -p data/circos/karyotype/
@@ -30,12 +31,12 @@ done
 for file in data/circos/fasta/*.fasta; do
   echo "run Prokka on $file"
   name=$(basename ${file%.*})
-  prokka --outdir data/circos/prokka/$name $file
+  prokka --outdir data/circos/prokka/$name $file >/dev/null 2>/dev/null
   mv data/circos/prokka/$name/*.gff data/circos/prokka/$name.gff 
 done
 
 # run prodigal and hmmsearch on input fasta files
-hmmvis/hmmvis/hmmvis --fasta_dir data/circos/fasta --output_dir data/circos/hmmvis/ --hmm data/hmm/model.hmm
+hmmvis/hmmvis/hmmvis --fasta_dir data/circos/fasta --output_dir data/circos/hmmvis/ --hmm data/hmm/model.hmm >/dev/null 2>/dev/null
 
 # generate circos files
 colors_pos=(paired-12-qual-1 paired-12-qual-3 paired-12-qual-5 paired-12-qual-7)
@@ -44,21 +45,21 @@ n=1
 for i in data/circos/hmmvis/*.fasta; do
   echo "processing: 'data/circos/fasta/'${i##*/}"
   ./generate_chr.sh 'data/circos/fasta/'${i##*/} ${colors_neg[@]:$n:1}
-  ./generate_gc.sh $i
-  ./generate_orf.sh $i
-  ./generate_hmm.sh $i.out
+  ./generate_gc.sh $i >/dev/null 2>/dev/null
+  ./generate_orf.sh $i >/dev/null 2>/dev/null
+  ./generate_hmm.sh $i.out >/dev/null 2>/dev/null
   n=$(($n+1))
 done
 
 for i in data/circos/prokka/*.gff; do
   # generate ORF locations
-  ./generate_orf_prokka.sh $i
+  ./generate_orf_prokka.sh $i >/dev/null 2>/dev/null
 done
 
 for i in data/circos/prokka/*.gff; do
   basename=${i##*/}
   name=${basename%.*}
- ./generate_coverage.sh $i data/circos/fasta/$name.fasta
+ ./generate_coverage.sh $i data/circos/fasta/$name.fasta >/dev/null 2>/dev/null
 done
 
 # multiple karyotypes
@@ -67,7 +68,27 @@ list_karyo=(data/circos/karyotype/*.txt)
 files=$(printf '%q,' "${list_karyo[@]}")
 sed -i "2s|.*|karyotype     = $files|" data/config/circos.conf
 
+echo "make blast search"
+
+# make a list 
+ls -1 data/circos/hmmvis/*.fasta > data/circos/blast/list.txt
+
+# step 1: pick one and run vs. all others
+
+firstline=$(head -n 1 data/circos/blast/list.txt)
+tail -n +2 data/circos/blast/list.txt > data/circos/blast/list2.txt # to prevent self-comparison
+while read line; do
+  echo "comparing $firstline with $line"
+  ./generate_blast.sh $firstline $line >/dev/null 2>/dev/null
+done <data/circos/blast/list2.txt
+
+# step 2: pick another one and run vs. all other without the first one picket
+
+
 # draw circos plot
-/root/software/circos/current/bin/circos -conf data/config/circos.conf -noparanoid
+/root/software/circos/current/bin/circos -conf data/config/circos.conf -noparanoid >/dev/null 2>/dev/null
 echo "finished"
+
+
+
 
