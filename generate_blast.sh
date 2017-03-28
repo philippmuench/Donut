29fca45b48f7724732ca30d7ad2d73eb6271ac13
anon <philipp.muench@helmholtz-hzi.de>
2017-03-28 12:33:27 +0200

    add blast output processing to circos format

--- generate_blast.sh
+++ generate_blast.sh
@@ -15,20 +15,53 @@ makeblastdb -in $a \
   -title $a2 \
   -out $a.blast.out
 
+
 # blast sample against one other
 blastp -query $b -db $a.blast.out \
   -out data/circos/blast/$b2.out \
   -evalue 0.00001 \
-  -outfmt '6 qseqid qstart qend sseqid sstart send ppos' 
+  -outfmt '6 qseqid qstart qend sseqid sstart send ppos'
+
 
 # search terms
-awk '{print ">"$1" "}' \
+awk '{print ">"$1"[[:space:]]"}' \
   data/circos/blast/$b2.out > /data/circos/blast/$b2.out.searchterms
 
-#while read f; do
-#  start_pos=$(grep $f $b | awk '{print $3}')
-#  end_pos=$(grep $f $b | awk '{print $4}')
-#  printf '%s\t%s\n' $start_pos $end_pos >> /data/circos/blast/$b2.start.tmp
-#done </data/circos/blast/$b2.out.searchterms
+while read f; do
+  start_pos=$(grep $f $b | awk '{print $3}')
+  end_pos=$(grep $f $b | awk '{print $5}')
+  echo "$f $start_pos $end_pos" >> data/circos/blast/$b2.pos
+done </data/circos/blast/$b2.out.searchterms
 
-# remove ORF information form query files
+# do grep ORF id from file
+awk '{print ">"$4"[[:space:]]"}' \
+  data/circos/blast/$b2.out > /data/circos/blast/$a2.out.searchterms
+
+while read f; do
+  start_pos=$(grep $f $a | awk '{print $3}')
+  end_pos=$(grep $f $a | awk '{print $5}')
+  echo "$f $start_pos $end_pos" >> data/circos/blast/$a2.pos
+done </data/circos/blast/$a2.out.searchterms
+
+# add ORF position to blast output position
+
+# process chr
+ data/circos/blast/$b2.out
+
+# merge to one big file
+paste  data/circos/blast/$b2.out \
+  data/circos/blast/$b2.pos data/circos/blast/$a2.pos > data/circos/blast/$b2.$a2.joined
+
+# sum columns
+cat data/circos/blast/$b2.$a2.joined \
+  | awk '{print $1" "$2+$9" "$3+$10" "$4" "$5+$9" "$6+$10}' > data/circos/blast/blast.$a2.$b2.tmp
+
+cat data/circos/blast/blast.$a2.$b2.tmp \
+  | awk '{sub(/\_/,"-",$1)};2' \
+  | awk '{sub(/\_/,"-",$4)};2' \
+  | awk '{sub(/\_/," ",$1)};2' \
+  | awk '{sub(/\_/," ",$5)};2' \
+  | awk '{print $1" "$3" "$4" "$5" "$7" "$8}' \
+  | awk '{sub(/\-/,"_",$1)};2' \
+  | awk '{sub(/\-/,"_",$4)};2' \
+  | sed 's/ /\t/g' > data/circos/blast/blast.txt
