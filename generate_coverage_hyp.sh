8c53fffaac0bb99093e66c92763369abf557e17f
anon <philipp.muench@helmholtz-hzi.de>
2017-03-29 12:27:50 +0200

    add script to generate hypothetical protein coverage

---
+++ generate_coverage_hyp.sh
@@ -0,0 +1,26 @@
+#/bin/sh
+# extract hypothetical genome coverage information using bedtools from Prokka output
+
+# $1 path to gff file
+# $2 fasta file
+
+# clean up gff file form fasta content
+cat $1 | sed '/^##FASTA$/,$d' \
+  | grep 'hypothetical' \
+  | sed '/^#/ d' > data/circos/coverage/${1##*/}_windows.bed.clean 
+
+# generate .genome file
+samtools faidx $2
+
+# generating windows
+bedtools makewindows -g $2.fai \
+  -w 10000 > data/circos/coverage/${2##*/}_windows.bed
+
+# extract coverage informations
+bedtools coverage -a data/circos/coverage/${2##*/}_windows.bed\
+   -b data/circos/coverage/${1##*/}_windows.bed.clean -counts > data/circos/coverage/${1##*/}_coverage.tmp
+
+# reformat
+awk '{print $1" " $2" "$3" "$4}' data/circos/coverage/${1##*/}_coverage.tmp > data/circos/coverage/${1##*/}_cov.txt
+
+cat data/circos/coverage/${1##*/}_cov.txt >> data/circos/coverage/cov_hyp.txt
